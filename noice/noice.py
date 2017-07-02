b4b7f7cc97318fe71c57f07bb0b8ceb7ddb94c7c
Justin Anderson <justin.ta@outlook.com>
2017-07-01 19:23:13 -0600

    License, setup, stub function

---
+++ noice/noice.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+The main thing
+'''
+
+def noice():
+    '''
+    The start to something... Well, something.
+
+    examples:
+        noice get cmd
+        noice create cmd <command>
+    '''
+    return 'make stuff'
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+# the new hotness
+# sem ver, obvi
+
+setup(name='noice',
+      version='0.1.0',
+      description='A way to save litle notes to recall later',
+      url='https://github.com/justinta/noice',
+      author='Justin Anderson',
+      author_email='justin.ta@outlook.com',
+      packages=['noice'],
+      license='Apache-2.0'
+      )
